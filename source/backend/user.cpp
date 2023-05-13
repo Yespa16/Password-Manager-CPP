7dca433f8b752859a3245ffa6fb64cf4f7639f2b
Yespa16 <codingyespa@gmail.com>
2023-05-13 23:09:34 +0400

    Update User class

--- source/backend/user.cpp
+++ source/backend/user.cpp
@@ -1,21 +1,25 @@
 #include "user.h"
 #include <string>
+#include "iostream"
 
 
-User::User() : username(""), password(""), pin(0){};
+User::User() : id(0), username(""), password(""), pin(""){};
 User::~User() {}
 
-User::User(std::string _u, std::string _p, std::string _pin) {
-    int v = validate(_u, _p, _pin);
+User::User(std::string _u, std::string _p, std::string _pin, int _id) {
+    int v = validate(_u, _p, _pin, _id);
     if (!v) {
+      this->id = _id;
       this->username = _u;
       this->password = hash(_p);
       this->pin = _pin;
+    }else{
+      std::cout << "Invalid Data \n";
     }
   }
 
-int User::validate(std::string username, std::string password, std::string pin) {
-    if (username.length() == 0 || password.length() == 0 || pin.length() == 0 || pin.length() > 4) {
+int User::validate(std::string username, std::string password, std::string pin, int id) {
+    if (username.length() == 0 || password.length() == 0 || pin.length() == 0 || pin.length() > 4 || id<0 ){
       return 1;
     }
     return 0;
@@ -25,16 +29,39 @@ std::string User::hash(std::string text) {
     // Implement hash algorithm later
     return text;
   }
-// class User {
 
-// private:
-//   std::string username;
-//   std::string password;
-//   int pin;
 
-// public:
-//   ~User() {
-//     username = nullptr;
-//     password = nullptr;
-//   }
-// };
+int User::get_id() {
+  return id;
+}
+
+std::string User::get_username() {
+  return username;
+}
+
+
+std::string User::get_password() {
+  return password;
+}
+
+
+std::string User::get_pin() {
+  return pin;
+}
+
+
+void User::set_id(int _id){
+  id = _id;
+}
+
+void User::set_username(const char* u){
+  username = u;
+}
+
+void User::set_password(const char* p) {
+  password = hash(p);
+}
+
+void User::set_pin(const char* _pin){
+  pin = _pin;
+}
--- source/backend/user.h
+++ source/backend/user.h
@@ -5,14 +5,26 @@ class User {
   std::string hash(std::string text);
 
 private:
+  int id;
   std::string username;
   std::string password;
   std::string pin;
 
 public:
     User();
-    User(std::string _u, std::string _p, std::string _pin);
+    User(std::string _u, std::string _p, std::string _pin, int _id=0);
     ~User();
 
-  int validate(std::string username, std::string password, std::string pin);
+    int get_id();
+    std::string get_username();
+    std::string get_password();
+    std::string get_pin();
+    
+    void set_id(int );
+    void set_username(const char*);
+    void set_password(const char*);
+    void set_pin(const char*);
+
+
+  int validate(std::string username, std::string password, std::string pin, int id);
 };
