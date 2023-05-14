beb247af817945f42ef4400b9d891c2d384e23d3
Yespa16 <codingyespa@gmail.com>
2023-05-14 20:12:45 +0400

    Update password class

--- source/backend/password.cpp
+++ source/backend/password.cpp
@@ -1,15 +1,14 @@
 #include "password.h"
 
 
+PasswordGroup::PasswordGroup(): id(0), user(nullptr), name("") {};
 
-PasswordGroup::PasswordGroup(): user(nullptr), name("") {};
-
-PasswordGroup::PasswordGroup(User* _user, std::string _name): user(_user), name(_name){};
+PasswordGroup::PasswordGroup(User* _user, std::string _name, int _id): id(_id), user(_user), name(_name){};
 
 PasswordGroup::~PasswordGroup() {user = nullptr;}
 
 void PasswordGroup::set_id(int _id){
-    id = _id;
+    this->id = _id;
 }
 
 int PasswordGroup::get_id(){
@@ -21,7 +20,11 @@ std::string PasswordGroup::get_name(){
 }
 
 void PasswordGroup::set_name(std::string _name) {
-    name = _name;
+    this->name = _name;
+}
+
+User* PasswordGroup::get_user() {
+    return user;
 }
 
 void PasswordGroup::set_user(User* u){
@@ -29,13 +32,16 @@ void PasswordGroup::set_user(User* u){
 }
 
 
-Password::Password(User* _u, std::string _email, std::string _username, PasswordGroup* _group, std::string _password) {
+Password::Password(): id(0), user(nullptr), group(nullptr), email(""), username(""), password(""){}
+
+Password::Password(User* _u,std::string _app,  std::string _email, std::string _username, PasswordGroup* _group, std::string _password, int _id ){
 
         if (_username.length() == 0 && _email.length() == 0 ) {
             std::cout <<  "Email and Username are empty, please provide at least one of them" << std::endl;
             //delete this;
             return;
         }
+        this->id = _id;
         this->user = _u;
         this->email = _email;
         this->username = _username;
@@ -50,3 +56,18 @@ Password::~Password(){
     }
 
 
+User* Password::get_user() {return user;}
+PasswordGroup* Password::get_group() {return group;}
+std::string Password::get_app() {return app;}
+std::string Password::get_email() {return email;}
+std::string Password::get_username() {return username;}
+std::string Password::get_password() {return password;}
+
+
+void Password::set_id(int _id){id = _id;}
+void Password::set_user(User* _u){user = _u; }
+void Password::set_group(PasswordGroup* p){group = p;}
+void Password::set_app(std::string _app) {app = _app;}
+void Password::set_email(std::string _email){email = _email;}
+void Password::set_username(std::string _u) {username = _u;}
+void Password::set_password(std::string _p){password = _p;}
--- source/backend/password.h
+++ source/backend/password.h
@@ -12,13 +12,14 @@ private:
 
 public:
     PasswordGroup();
-    PasswordGroup(User* _user, std::string _name);
+    PasswordGroup(User* _user, std::string _name, int _id = 0);
     ~PasswordGroup();
 
 
     int get_id();
     void set_id(int _id);
     void set_user(User*);
+    User* get_user();
     std::string get_name();
     void set_name(std::string);
 };
@@ -26,14 +27,32 @@ public:
 
 class Password{
 private:
+    int id;
     User* user;
+    PasswordGroup* group;
+    std::string app;
     std::string email;
     std::string username;
-    PasswordGroup* group;
     std::string password;
 
 public:
-    Password(User* _u, std::string _email, std::string _username, PasswordGroup* _group, std::string _password);
+    Password();
+    Password(User* _u,std::string _app,  std::string _email, std::string _username, PasswordGroup* _group, std::string _password, int _id = 0);
     ~Password();
-    
+
+    User* get_user();
+    PasswordGroup* get_group();
+    std::string get_app();
+    std::string get_email();
+    std::string get_username();
+    std::string get_password();
+
+    void set_id(int);
+    void set_user(User*);
+    void set_group(PasswordGroup*);
+    void set_app(std::string);
+    void set_email(std::string);
+    void set_username(std::string);
+    void set_password(std::string);
+
 };
