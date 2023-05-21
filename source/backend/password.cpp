c925243f8ea75e88d278dabf051191ae4663a4b5
Yespa16 <codingyespa@gmail.com>
2023-05-21 13:54:35 +0400

    Change Password classes

--- source/backend/password.cpp
+++ source/backend/password.cpp
@@ -1,130 +1,120 @@
 #include "password.h"
 
-PasswordGroup::PasswordGroup() : id(0), user(nullptr), name(""){};
+Validator Validate;
 
-PasswordGroup::PasswordGroup(User *_user, std::string _name, int _id) : id(_id), user(_user), name(_name){};
+Password::Password() : id(0), user(nullptr), app(""), password("") {}
 
-PasswordGroup::~PasswordGroup()
+Password::Password(User *_u, std::string _app,  std::string _password, int _id)
 {
-    user = nullptr;
-}
-
-void PasswordGroup::set_id(int _id)
-{
-    this->id = _id;
-}
-
-int PasswordGroup::get_id()
-{
-    return id;
-}
-
-std::string PasswordGroup::get_name()
-{
-    return name;
-}
-
-void PasswordGroup::set_name(std::string _name)
-{
-    this->name = _name;
-}
-
-User *PasswordGroup::get_user()
-{
-    return user;
-}
-
-void PasswordGroup::set_user(User *u)
-{
-    user = u;
-}
-
-Password::Password() : id(0), user(nullptr), group(nullptr), email(""), username(""), password("") {}
-
-Password::Password(User *_u, std::string _app, std::string _email, std::string _username, PasswordGroup *_group, std::string _password, int _id)
-{
-
-    if (_username.length() == 0 && _email.length() == 0)
-    {
-        std::cout << "Email and Username are empty, please provide at least one of them" << std::endl;
-        // delete this;
-        return;
+    if (_id <= 0) {
+        throw std::logic_error("Id must be greater than 0");
     }
     this->id = _id;
     this->user = _u;
-    this->email = _email;
-    this->username = _username;
-    this->group = _group;
-    this->password = _password;
+    this->app = std::move(_app);
+
+    if(_password.length() == 0){
+        throw std::logic_error("Password must be not empty");
+    }
+    this->password = std::move(_password);
+
+    }
+void Password::set_id( int _id) {
+        if (_id <= 0) {
+            throw std::logic_error("Id must be greater than 0");
+        }
+        this->id = _id;
+    }
+void Password::set_password(std::string _password) {
+        if (_password.length() == 0) {
+            throw std::logic_error("Password must be not empty");
+        }
+        this->password = std::move(_password);
 }
 
-Password::~Password()
-{
-    user = nullptr;
-    group = nullptr;
+
+std::string Password::add_sql() {
+    
+    std::string user_id = std::to_string(get_user()->get_id());
+    std::string app = get_app();
+    std::string password = get_password();
+
+    std::string command = "INSERT INTO password (user_id, app, email, username, password) VALUES(" + user_id + ", '" + app + "', '', '', '" + password + "');";
+    return command;
 }
 
-User *Password::get_user()
-{
-    return user;
+
+
+EmailPassword::EmailPassword():Password(), email(""), phone_number(""), reserve_email(""){}
+
+
+EmailPassword::EmailPassword(User *_u, std::string _app,  std::string _password, int _id, std::string email, std::string phone_number, std::string reserve_email){
+    Password(_u, _app, _password, _id);
+    Validate.Email(email);
+    this->email = std::move(email);
+    this->phone_number = std::move(phone_number);
+    this->reserve_email = std::move(reserve_email);
 }
 
-PasswordGroup *Password::get_group()
-{
-    return group;
+
+void EmailPassword::set_email(std::string email) {
+    Validate.Email(email);
+    this->email = std::move(email);
 }
 
-std::string Password::get_app()
-{
-    return app;
+
+void EmailPassword::set_phone_number(std::string phone_number){
+    this->phone_number = std::move(phone_number);
 }
 
-std::string Password::get_email()
-{
-    return email;
+void EmailPassword::set_reserve_email(std::string reserve){
+    this->reserve_email = std::move(reserve);
 }
 
-std::string Password::get_username()
-{
-    return username;
+
+
+std::string EmailPassword::add_sql(){
+
+    std::string user_id = std::to_string(get_user()->get_id());
+    std::string app = get_app();
+    std::string password = get_password();
+
+    std::string command = "INSERT INTO password (user_id, app, email, username, password, phone_nubmer, reserve_email) VALUES(" + user_id +  ", '" + app + "', '" + email + "', '" + "'', " + "', '" + password + "', '" + phone_number + "', '" + reserve_email +  " ');";
+    return command;
 }
 
-std::string Password::get_password()
-{
-    return password;
+
+
+GamePassword::GamePassword():Password(), username(""), email(""), level(""), description(""){}
+
+
+GamePassword::GamePassword(User *_u, std::string _app,  std::string _password, int _id, std::string _username, std::string _email, std::string _level, std::string _description){
+    Password(_u, _app, _password, _id);
+    Validate.UsernameAndEmail(_username, _email);
+    this->username = std::move(_username);
+    this->email = std::move(_email);
+    this->level = std::move(_level);
+    this->description = std::move(_description);    
 }
 
-void Password::set_id(int _id)
-{
-    id = _id;
+
+void GamePassword::set_username(std::string username){
+    Validate.Username(username);
+    this->username = std::move(username);
 }
 
-void Password::set_user(User *_u)
-{
-    user = _u;
+
+void GamePassword::set_email(std::string email){
+    Validate.Email(email);
+    this->email = std::move(email);
 }
 
-void Password::set_group(PasswordGroup *p)
-{
-    group = p;
-}
 
-void Password::set_app(std::string _app)
-{
-    app = _app;
-}
-
-void Password::set_email(std::string _email)
-{
-    email = _email;
-}
-
-void Password::set_username(std::string _u)
-{
-    username = _u;
-}
-
-void Password::set_password(std::string _p)
-{
-    password = _p;
+std::string GamePassword::add_sql(){
+    
+    std::string user_id = std::to_string(get_user()->get_id());
+    std::string app = get_app();
+    std::string password = get_password();
+    std::string command = "INSERT INTO password (user_id, app, email, username, password, phone_nubmer, reserve_email, level, description) VALUES(" + user_id +  ", '" + app + "', '" + email + "', '" + username + "', '" + password + "', '" + "'', " + "', '" + "'', " +  "', '" + level + "', '" + description + " ');";
+    return command;
 }
--- source/backend/password.h
+++ source/backend/password.h
@@ -1,55 +1,83 @@
 #include <string>
 #include <iostream>
 #include "user.h"
+#include "validator.h"
 
-class PasswordGroup
-{
-private:
-    int id;
-    User *user;
-    std::string name;
 
-public:
-    PasswordGroup();
-    PasswordGroup(User *_user, std::string _name, int _id = 0);
-    ~PasswordGroup();
-
-    int get_id();
-    void set_id(int _id);
-    void set_user(User *);
-    User *get_user();
-    std::string get_name();
-    void set_name(std::string);
-};
 
 class Password
 {
 private:
     int id;
-    User *user;
-    PasswordGroup *group;
+    User* user;
     std::string app;
-    std::string email;
-    std::string username;
     std::string password;
+
 
 public:
     Password();
-    Password(User *_u, std::string _app, std::string _email, std::string _username, PasswordGroup *_group, std::string _password, int _id = 0);
-    ~Password();
+    Password(User *_u, std::string _app, std::string _password, int _id = 0);
+    ~Password(){}
 
-    User *get_user();
-    PasswordGroup *get_group();
-    std::string get_app();
-    std::string get_email();
-    std::string get_username();
-    std::string get_password();
+    User* get_user(){return this->user;}
+    std::string& get_app() {return this->app;}
+    std::string& get_password() {return this->password;}
 
-    void set_id(int);
-    void set_user(User *);
-    void set_group(PasswordGroup *);
-    void set_app(std::string);
-    void set_email(std::string);
-    void set_username(std::string);
-    void set_password(std::string);
+    void set_id(int _id);
+    void set_user(User* _u) {this->user = _u;}
+    void set_app(std::string _app) {this->app = std::move(_app);}
+    void set_password(std::string _password);
+
+    virtual std::string add_sql();
+};
+
+
+
+class EmailPassword: public Password{ 
+private:
+    std::string email;
+    std::string phone_number;
+    std::string reserve_email;
+
+public:
+    EmailPassword();
+    EmailPassword(User *_u, std::string _app,  std::string _password, int _id, std::string email, std::string phone_number, std::string reserve_email);
+    ~EmailPassword(){}
+
+    std::string& get_email() {return this->email;}
+    std::string& get_phone_number() {return this->phone_number;}
+    std::string& get_reserve_email() {return this->reserve_email;}
+
+    void set_email(std::string email);
+    void set_phone_number(std::string phone_number);
+    void set_reserve_email(std::string reserve);
+
+    virtual std::string add_sql();
+};
+
+
+
+class GamePassword: public Password{ 
+private:
+    std::string username;
+    std::string email;
+    std::string level;
+    std::string description;
+
+public:
+    GamePassword();
+    GamePassword(User *_u, std::string _app,  std::string _password, int _id, std::string _username, std::string _email, std::string _level, std::string _description);
+    ~GamePassword(){}
+
+    std::string& get_username() {return this->username;}
+    std::string& get_level() {return this->level;}
+    std::string& get_email() {return this->email;}
+    std::string& get_description() {return this->description;}
+
+    void set_username(std::string username);
+    void set_email(std::string email);
+    void set_level(std::string level) {this->level = std::move(level);}
+    void set_description(std::string description) {this->description = std::move(description);}
+
+    virtual std::string add_sql();
 };
